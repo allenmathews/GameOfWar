8eb13ae8eb2dd52c542d868ae1592a32896e30d6
Allen Mathews <allenjoemathews@gmail.com>
2022-10-24 20:06:49 +1100

    display  the winner

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ drawCardBtn.addEventListener("click", () => {
         })
 })
 
+
 function determineCardWinner(card1, card2) {
     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9",
         "10", "JACK", "QUEEN", "KING", "ACE"

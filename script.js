c31bd647364b4a74980d92974f2bab6387304d7f
Allen Mathews <allenjoemathews@gmail.com>
2022-10-25 20:10:47 +1100

    refactored the script

--- script.js
+++ script.js
@@ -1,63 +1,45 @@
-let deckId
-let computerScore = 0
-let myScore = 0
-const cardsContainer = document.getElementById("cards")
-const newDeckBtn = document.getElementById("new-deck")
-const drawCardBtn = document.getElementById("draw-cards")
-const header = document.getElementById("header")
-const remainingText = document.getElementById("remaining")
-const computerScoreEl = document.getElementById("computer-score")
-const myScoreEl = document.getElementById("my-score")
+let postsArray = []
 
-function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
-            deckId = data.deck_id
-            console.log(deckId)
-        })
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
 }
 
-newDeckBtn.addEventListener("click", handleClick)
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        postsArray = data.slice(0, 5)
+        renderPosts()
+    })
 
-drawCardBtn.addEventListener("click", () => {
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
-        .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
-            cardsContainer.children[0].innerHTML = `
-                <img src=${data.cards[0].image} class="card" />
-            `
-            cardsContainer.children[1].innerHTML = `
-                <img src=${data.cards[1].image} class="card" />
-            `
-            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
-            header.textContent = winnerText
-
-            if (data.remaining === 0) {
-                drawCardBtn.disabled = true
-            }
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
         })
 })
-
-
-function determineCardWinner(card1, card2) {
-    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9",
-        "10", "JACK", "QUEEN", "KING", "ACE"
-    ]
-    const card1ValueIndex = valueOptions.indexOf(card1.value)
-    const card2ValueIndex = valueOptions.indexOf(card2.value)
-
-    if (card1ValueIndex > card2ValueIndex) {
-        computerScore++
-        computerScoreEl.textContent = `Computer score: ${computerScore}`
-        return "Computer wins!"
-    } else if (card1ValueIndex < card2ValueIndex) {
-        myScore++
-        myScoreEl.textContent = `My score: ${myScore}`
-        return "You win!"
-    } else {
-        return "War!"
-    }
-}

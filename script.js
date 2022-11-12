008967bf1f75498b6a332ff9a96c2caa8c56fa6b
Allen Mathews <allenjoemathews@gmail.com>
2022-11-12 20:51:03 +1100

    keep score

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
             console.log(deckId)
         })
 }
 
 newDeckBtn.addEventListener("click", handleClick)
+
 
 
 

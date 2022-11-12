d82c5df8de1776636a0f0413e3a0968c0c5b106f
Allen Mathews <allenjoemathews@gmail.com>
2022-11-12 20:49:34 +1100

    added game feature

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 let deckId
-let computerScore = 0
-let myScore = 0
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
-const header = document.getElementById("header")
-const remainingText = document.getElementById("remaining")
-const computerScoreEl = document.getElementById("computer-score")
-const myScoreEl = document.getElementById("my-score")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
-            console.log(deckId)
         })
 }
 
@@ -25,21 +17,15 @@ drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
-            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
-            header.textContent = winnerText
-
-            if (data.remaining === 0) {
-                drawCardBtn.disabled = true
-            }
         })
 })
+
 
 function determineCardWinner(card1, card2) {
     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9",
@@ -47,16 +33,14 @@ function determineCardWinner(card1, card2) {
     ]
     const card1ValueIndex = valueOptions.indexOf(card1.value)
     const card2ValueIndex = valueOptions.indexOf(card2.value)
+    console.log("card 1:", card1ValueIndex)
+    console.log("card 2:", card2ValueIndex)
 
     if (card1ValueIndex > card2ValueIndex) {
-        computerScore++
-        computerScoreEl.textContent = `Computer score: ${computerScore}`
-        return "Computer wins!"
+        console.log("Card 1 wins!")
     } else if (card1ValueIndex < card2ValueIndex) {
-        myScore++
-        myScoreEl.textContent = `My score: ${myScore}`
-        return "You win!"
+        console.log("Card 2 wins!")
     } else {
-        return "War!"
+        console.log("It's a tie!")
     }
 }

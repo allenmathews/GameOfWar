9caf3ef5bd84bac3b53b8108588a93c7bd430cf1
Allen Mathews <allenjoemathews@gmail.com>
2023-08-13 13:39:53 +1000

    added remaining card

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
 const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+
             deckId = data.deck_id
-            console.log(deckId)
+                // console.log(deckId)
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
         })
 }
 
 newDeckBtn.addEventListener("click", handleClick)
 
+
 drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
@@ -27,9 +32,12 @@ drawCardBtn.addEventListener("click", () => {
             `
             const winnerText = determineCardWinner(data.cards[0], data.cards[1])
             header.textContent = winnerText
+
+            if (data.remaining === 0) {
+                drawCardBtn.disabled = true;
+            }
         })
 })
-
 
 function determineCardWinner(card1, card2) {
     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9",
